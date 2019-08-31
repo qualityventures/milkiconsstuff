2552571bb0f72e05ee982647d9b200172e7e45cd
Peter <contact@quality.ventures>
2019-08-31 19:28:35 +0000

    first commit

---
+++ client/precache-manifest.c3aa8f5ef23e867389786cb774b65940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63a9586d5d813cb41b15",
+    "url": "/static/js/main.b18bdea0.chunk.js"
+  },
+  {
+    "revision": "393c3bb454c1d4e15061",
+    "url": "/static/js/2.6716d59d.chunk.js"
+  },
+  {
+    "revision": "63a9586d5d813cb41b15",
+    "url": "/static/css/main.1a37dc15.chunk.css"
+  },
+  {
+    "revision": "e75c46792aa5e9106940eefce65a9988",
+    "url": "/index.html"
+  }
+];
