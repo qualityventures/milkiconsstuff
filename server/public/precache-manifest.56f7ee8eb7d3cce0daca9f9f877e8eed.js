d8ce0648b1a1afee1565550b376f2a5a00b8deb7
guangwen1221 <guangwen1221@gmail.com>
2019-09-12 08:14:06 +0800

    update frontend code

---
+++ server/public/precache-manifest.56f7ee8eb7d3cce0daca9f9f877e8eed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d8b96d1ecca1ee1b5f4",
+    "url": "/static/js/main.d76f7810.chunk.js"
+  },
+  {
+    "revision": "71254d25e3d493b91353",
+    "url": "/static/js/2.2c3f8f0d.chunk.js"
+  },
+  {
+    "revision": "4d8b96d1ecca1ee1b5f4",
+    "url": "/static/css/main.7ac6b10d.chunk.css"
+  },
+  {
+    "revision": "9ea6c44a92df380c51ac09fa99e1d56c",
+    "url": "/index.html"
+  }
+];
